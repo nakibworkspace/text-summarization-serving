8f7b0a3882f2db3aac74ba7002d7ca3f2db97c55
nakibworkspace <136246362+nakibworkspace@users.noreply.github.com>
2026-01-19 12:50:02 +0600

    Local changes to ping and config

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -1,18 +1,7 @@
-# project/app/api/ping.py
+from fastapi import FastAPI
 
+app = FastAPI()
 
-from fastapi import APIRouter, Depends
-
-from app.config import Settings, get_settings
-
-
-router = APIRouter()
-
-
-@router.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+@app.get("/ping")
+def pong():
+    return {"ping": "pong!"}
